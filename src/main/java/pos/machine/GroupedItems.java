5b3e355d815ff3f75cb85aabc27d450482bd84fe
Marie Chow <chowma3@oocl.com>
2022-11-29 17:23:41 +0800

    feat:modify GroupedItems class

--- src/main/java/pos/machine/GroupedItems.java
+++ src/main/java/pos/machine/GroupedItems.java
@@ -8,42 +8,32 @@ public class GroupedItems {
     String name;
     int quantity;
     int unitPrice;
+    int subtotalPrice;
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public int getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
     public int getUnitPrice() {
         return unitPrice;
     }
 
-    public void setUnitPrice(int unitPrice) {
-        this.unitPrice = unitPrice;
-    }
-
-    int getSubtotalPrice() {
-        return quantity * unitPrice;
+    public int getSubtotalPrice() {
+        return subtotalPrice;
     }
 
     public GroupedItems(String name, int quantity, int unitPrice) {
         this.name = name;
         this.quantity = quantity;
         this.unitPrice = unitPrice;
+        this.subtotalPrice = quantity * unitPrice;
     }
 
-    List<GroupedItems> fromItems(List<Item> items) {
+    public static List<GroupedItems> fromItems(List<Item> items) {
         List<GroupedItems> groupedItems = new ArrayList<>();
 
         List<String> addedBarcodes = new ArrayList<>();
@@ -57,7 +47,7 @@ public class GroupedItems {
 
         return groupedItems;
     }
-    int getQuantity(String barcode, List<Item> items) {
+    static int getQuantity(String barcode, List<Item> items) {
         return items.stream().filter(item -> item.getBarcode().equals(barcode)).collect(Collectors.toList()).size();
     }
 }
