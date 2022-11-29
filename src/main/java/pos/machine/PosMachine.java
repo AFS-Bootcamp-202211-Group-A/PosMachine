cf0426d088d01526d03b6c0cbda2e6049af27f32
WONGJE4 <wongje4@oocl.com>
2022-11-29 21:27:15 +0800

    feat: finish getEachItemNamePriceQuantity

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -3,6 +3,10 @@ package pos.machine;
 import java.util.List;
 
 public class PosMachine {
+
+    public List<Item> getEachItemNamePriceQuantity(List<String> barcodes){
+        return ItemDataLoader.loadAllItems();
+    }
     public String printReceipt(List<String> barcodes) {
         return null;
     }
