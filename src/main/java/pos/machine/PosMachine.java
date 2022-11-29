2501ab00819e9907ae97ddb9d4b5e11b3fed2dbd
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-11-29 17:54:01 +0800

    feat: finish aggregateBarcodes

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,26 @@
 package pos.machine;
 
 import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         return null;
+    }
+
+    public ArrayList<BarcodeCount> aggregateBarcodes(ArrayList<String> barcodes){
+        ArrayList<BarcodeCount> barcodeCountList = new ArrayList<BarcodeCount>();
+        HashMap<String, BarcodeCount> barcodeCounter = new HashMap<>();
+        for(String barcode: barcodes){
+            if (barcodeCounter.containsKey(barcodes)){
+                barcodeCounter.get(barcode).setQuantity(barcodeCounter.get(barcode).getQuantity()+1);
+            } else {
+                BarcodeCount barcodeCount= new BarcodeCount(barcode, 1);
+                barcodeCounter.put(barcode, barcodeCount);
+                barcodeCountList.add(barcodeCount);
+            }
+        }
+        return barcodeCountList;
     }
 }
