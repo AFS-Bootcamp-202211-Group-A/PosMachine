4df0861f658f8a2cc70292dfaea884eb31e4b190
Marie Chow <chowma3@oocl.com>
2022-11-29 22:02:48 +0800

    feat: implement getTotalPrice

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,19 @@
 package pos.machine;
 
+import java.security.acl.Group;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         return null;
+    }
+    int getTotalPrice(List<GroupedItems> groupedItems) {
+        int totalPrice = 0;
+        for (GroupedItems groupedItem : groupedItems) {
+            totalPrice += groupedItem.getSubtotalPrice();
+        }
+        return totalPrice;
     }
 }
