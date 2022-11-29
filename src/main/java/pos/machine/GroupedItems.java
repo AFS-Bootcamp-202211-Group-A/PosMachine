448f16e16ab3a7331542b10a19506229ec174371
Marie Chow <chowma3@oocl.com>
2022-11-29 17:10:53 +0800

    feat: implement conversion to GroupedItems

--- src/main/java/pos/machine/GroupedItems.java
+++ src/main/java/pos/machine/GroupedItems.java
@@ -1,5 +1,9 @@
 package pos.machine;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class GroupedItems {
     String name;
     int quantity;
@@ -31,5 +35,29 @@ public class GroupedItems {
 
     int getSubtotalPrice() {
         return quantity * unitPrice;
+    }
+
+    public GroupedItems(String name, int quantity, int unitPrice) {
+        this.name = name;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
+
+    List<GroupedItems> fromItems(List<Item> items) {
+        List<GroupedItems> groupedItems = new ArrayList<>();
+
+        List<String> addedBarcodes = new ArrayList<>();
+
+        for (Item item : items) {
+            if (addedBarcodes.contains(item.getBarcode())) {
+                continue;
+            }
+            groupedItems.add(new GroupedItems(item.getName(), getQuantity(item.getBarcode(), items), item.getPrice()));
+        }
+
+        return groupedItems;
+    }
+    int getQuantity(String barcode, List<Item> items) {
+        return items.stream().filter(item -> item.getBarcode().equals(barcode)).collect(Collectors.toList()).size();
     }
 }
