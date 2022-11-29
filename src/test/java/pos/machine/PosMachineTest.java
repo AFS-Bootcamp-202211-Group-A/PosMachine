888832a3db97f5f340c70f5f55a6e450018bfccf
heinrich-oocl <117629815+heinrich-oocl@users.noreply.github.com>
2022-11-29 17:53:34 +0800

    test: add all test cases

--- src/test/java/pos/machine/PosMachineTest.java
+++ src/test/java/pos/machine/PosMachineTest.java
@@ -2,15 +2,14 @@ package pos.machine;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PosMachineTest {
 
     @Test
-    public void should_return_map() {
+    public void should_return_correct_quantity() {
         PosMachine posMachine = new PosMachine();
 
         Map<String, Integer> expectedMap = new HashMap<String, Integer>() {{
@@ -20,6 +19,49 @@ public class PosMachineTest {
         }};
 
         assertEquals(expectedMap, posMachine.loadItemQuantity(ItemDataLoader.loadBarcodes()));
+    }
+
+    @Test
+    public void should_return_items() {
+        PosMachine posMachine = new PosMachine();
+
+        ArrayList<String> itemsCode = new ArrayList<>();
+        itemsCode.add("ITEM000000");
+        itemsCode.add("ITEM000001");
+
+
+        List<Item> filteredResult =  posMachine.getAndFilterItems(itemsCode);
+
+        assertEquals(filteredResult.size(), 2);
+        Item sample = filteredResult.get(0);
+
+        assertEquals(sample.getBarcode(),"ITEM000000");
+        assertEquals(sample.getName(), "Coca-Cola");
+        assertEquals(sample.getPrice(), 3);
+    }
+
+
+    @Test
+    public void should_return_receipt_line() {
+        PosMachine posMachine = new PosMachine();
+        String result =  posMachine.generateReceiptLine(new Item("ITEM000000", "Coca-Cola", 3), 5);
+
+        assertEquals("Name: Coca-Cola, Quantity: 5, Unit price: 3 (yuan), Subtotal: 15 (yuan)\n",result);
+    }
+
+    @Test
+    public void should_return_total_price() {
+        PosMachine posMachine = new PosMachine();
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new Item("ITEM000000", "Coca-Cola", 3));
+        items.add(new Item("ITEM000001", "Sprite", 3));
+        Map<String, Integer> quantityMap = new HashMap<String, Integer>() {{
+            put("ITEM000000", 4);
+            put("ITEM000001", 2);
+        }};
+        int result =  posMachine.getTotalPrice(items, quantityMap);
+
+        assertEquals(18, result);
     }
 
     @Test
