7aff7ba6b69a57e42a738c0ff23598cbcd4f8c3b
Alan Wan <alan.wan@oocl.com>
2022-11-29 17:55:08 +0800

    feat: added printReceipt

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,14 @@
 package pos.machine;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
-        return null;
+        String result = constructRecipt(barcodes);
+        return result;
     }
+    
+
 }
