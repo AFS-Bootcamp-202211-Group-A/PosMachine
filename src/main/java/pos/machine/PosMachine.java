afaec281672bc79570b22ed5c8959fbcaa5ab4bf
Marie Chow <chowma3@oocl.com>
2022-11-29 22:03:22 +0800

    feat: implement generateItemString

--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -9,6 +9,9 @@ public class PosMachine {
     public String printReceipt(List<String> barcodes) {
         return null;
     }
+    String generateItemString(GroupedItems groupedItem) {
+        return "Name: " + groupedItem.getName() + ", Quantity: " + groupedItem.getQuantity() + ", Unit price: " + groupedItem.getUnitPrice() + " (yuan), Subtotal: " + groupedItem.getSubtotalPrice() + " (yuan)\n";
+    }
     int getTotalPrice(List<GroupedItems> groupedItems) {
         int totalPrice = 0;
         for (GroupedItems groupedItem : groupedItems) {
