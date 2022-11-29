d0237856e131a811140083e82d395c60a3ca9271
Marie Chow <chowma3@oocl.com>
2022-11-29 16:53:55 +0800

    feat: create GroupedItems class

---
+++ src/main/java/pos/machine/GroupedItems.java
@@ -0,0 +1,35 @@
+package pos.machine;
+
+public class GroupedItems {
+    String name;
+    int quantity;
+    int unitPrice;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(int unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    int getSubtotalPrice() {
+        return quantity * unitPrice;
+    }
+}
